735ca8a281cc0e0c869df658e8065c2f04708f39
Adil Naimi <adilnm23@gmail.com>
2020-06-02 23:36:29 -0500

    Set up redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/new';
+
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+ 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/new.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+
+const rootReducer=combineReducers({
+    
+})
+export default rootReducer
+
+function Add(state=[],action){
+    switch (action.type) {
+        case 'CREATE-POST':
+            return [...state,action.payload]
+        default:
+           return state
+    }
+}
