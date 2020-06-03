00e31ac0c9af500ad35177b68475e2386916f724
Adil Naimi <adilnm23@gmail.com>
2020-06-03 00:54:25 -0500

    create handleChange function

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import newPost from '../actions/newPost'
+import React from "react";
+import { connect } from "react-redux";
+import newPost from "../actions/newPost";
 
-class PostCreate extends React.Component{
-    state = {
-        title:'',
-        body:'',
-        published: flase
-      }
-   
-    render(){
-        return(
-            <div>PostCreate</div>
-        )
-    }
+class PostCreate extends React.Component {
+  state = {
+    title: "",
+    body: "",
+    published: false
+  };
+
+  handleChange = e => {
+    this.setState({
+        [e.target.name]:e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+          <br />
+          <textarea
+            onChange={this.handleChange}
+            name="content"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Body"
+          />
+          <br />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default connect(null,{newPost})(PostCreate)
+export default connect(null, { newPost })(PostCreate);
