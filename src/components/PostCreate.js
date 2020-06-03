86ba4c1f9e524260889c8e599fc1a255a0496410
Adil Naimi <adilnm23@gmail.com>
2020-06-02 23:55:44 -0500

    create state

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import newPost from '../actions/newPost'
 
 class PostCreate extends React.Component{
+    state = {
+        title:'',
+        body:'',
+        published: flase
+      }
+   
     render(){
         return(
             <div>PostCreate</div>
