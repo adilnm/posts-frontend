3e5289ad88b85f836e0e389716c20dc371a83b26
Adil Naimi <adilnm23@gmail.com>
2020-06-02 23:40:16 -0500

    upload the reducer

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const store = createStore(
  
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
--- src/reducers/new.js
+++ src/reducers/new.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
 const rootReducer=combineReducers({
-    
+    Posts:Add
 })
 export default rootReducer
 
