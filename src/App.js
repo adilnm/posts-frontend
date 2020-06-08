d3ec1296cdc739195f8c2aa53e932573c83fc1f0
Adil Naimi <adilnm23@gmail.com>
2020-06-07 23:42:26 -0500

    Switch the App components to a class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import PostCreate from './components/PostCreate'
-import PostDisplay from './components/PostDisplay';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PostCreate from "./components/PostCreate";
+import PostDisplay from "./components/PostDisplay";
 
-
-function App() {
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <Router>
         <div>
           <Route exact path="/posts/new" component={PostCreate} />
           <Route exact path="/posts" component={PostDisplay} />
         </div>
       </Router>
-    )
+    );
+  }
 }
 
 export default App;
