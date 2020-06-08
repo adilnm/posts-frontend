44c1eb7e3faa35971738eda3cd001010146e48ef
Adil Naimi <adilnm23@gmail.com>
2020-06-07 23:27:16 -0500

    Create PostDisplay component

---
+++ src/components/PostDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PostDisplay extends React.component{
+    render(){
+        return(
+            <div>
+                PostDisplay
+            </div>
+        )
+    }
+}
+
+export default  PostDisplay
