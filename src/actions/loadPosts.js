eb50abb02166b8ab6c9d5c09c5ea41b176137037
Adil Naimi <adilnm23@gmail.com>
2020-06-07 23:39:41 -0500

    create an action to load all the different posts from the database

---
+++ src/actions/loadPosts.js
@@ -0,0 +1,13 @@
+export default function() {
+  return dispatch => {
+    fetch("http://localhost:3001/posts", {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+    .then(res=>res.json())
+    
+  };
+}
